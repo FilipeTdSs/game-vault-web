6ceecafac11b25735e6454326898b42e26b981b8
Filipe <filipe.t.santos@hotmail.com>
2026-09-24 17:33:59 -0300

    chore: setup do Angular 20 zoneless com Tailwind 4 e ngx-translate

---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideTranslateService({
+      fallbackLang: 'pt',
+      lang: 'pt',
+    }),
+    provideTranslateHttpLoader({
+      prefix: './i18n/',
+      suffix: '.json',
+    }),
+  ],
+};
